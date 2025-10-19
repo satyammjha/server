3d42bbbe97f6ccf324e61f367e5740941ac0b82c
satyammjha <satyammjha0@gmail.com>
2025-10-20 00:07:43 +0530

    added password to redis

--- queue/queue.ts
+++ queue/queue.ts
@@ -6,6 +6,7 @@ const redisConfig = {
   connection: {
     host: process.env.REDIS_HOST || "127.0.0.1",
     port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT, 10) : 6379,
+    password: 'dollar$babydollar$'
   },
 };
 
