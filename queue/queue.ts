42fff21904fcd2ff8636035715ed523b6a81fc71
satyammjha <satyammjha0@gmail.com>
2025-12-11 15:24:53 +0530

    updated redis IP

--- queue/queue.ts
+++ queue/queue.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const redisConfig = {
   connection: {
-    host: '43.204.255.255',
+    host: '13.204.102.186',
     port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT, 10) : 6379,
     password: 'dollar$babydollar$'
   },
