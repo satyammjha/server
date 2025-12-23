b1c22c3fcb15343a541f7f058cd4818e99ca2441
satyammjha <satyammjha0@gmail.com>
2025-12-23 16:20:36 +0530

    added reqTracker

--- queue/queue.ts
+++ queue/queue.ts
@@ -7,6 +7,7 @@ dotenv.config();
 export const redis = new IORedis({
   host: process.env.REDIS_HOST || "127.0.0.1",
   port: Number(process.env.REDIS_PORT) || 6379,
+  password: process.env.REDIS_PASSWORD || '$dollarbabydollar$', 
   maxRetriesPerRequest: null,
 });
 
